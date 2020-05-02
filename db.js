8a36b1714162db14d845a583ce0e40094cdcfaf2
Ganesh Avhad <ganesh.avhad@wohlig.com>
2020-05-02 20:59:27 +0530

    variable setup changed

--- db.js
+++ db.js
@@ -4,11 +4,12 @@ const config = require('config');
 module.exports = function () {
   console.log("Process", process.env.NODE_ENV)
   const dbName = config.get('dbName');
-  let db = `mongodb://localhost:27017/${dbName}`
   if (process.env.NODE_ENV == 'production') {
     const dbPassword = config.get('dbPassword');
     const dbUserName = config.get('dbUserName');
     let db = `mongodb+srv://${dbUserName}:${dbPassword}@cluster0-mieym.mongodb.net/${dbName}?retryWrites=true&w=majority`
+  } else {
+    let db = `mongodb://localhost:27017/${dbName}`
   }
   console.log("database url", db)
   mongoose.connect(db, {
